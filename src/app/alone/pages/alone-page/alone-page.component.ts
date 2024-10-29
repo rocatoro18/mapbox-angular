5cf8b4b757d6177ddb514ed64b38b2fe8c128fd5
Roberto Tores <rocatoro18@outlook.com>
2024-10-29 15:11:54 -0700

    .

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CounterAloneComponent } from "../../components/counter-alone/counter-alone.component";
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CounterAloneComponent],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css']
 })
 export class AlonePageComponent {
+
+  constructor(){}
 
 }
